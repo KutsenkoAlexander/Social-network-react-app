b9b07b123001d734a5bed4e6cc2a76af7cbeb8d1
KutsenkoAlexander <kucenkoaleksandr@gmail.com>
2021-02-03 19:15:16 +0200

    adding CSS modules

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Header.module.css'
+import './Header.css';
 
 function Header() {
     return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import './Profile.css';
 
 function Profile() {
-    return <div className="App-content">
+    return <div className="App-profile">
         <div>
             <img
                 src="https://images.unsplash.com/photo-1496889748357-08060ad87d28?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2124&q=80"/>
