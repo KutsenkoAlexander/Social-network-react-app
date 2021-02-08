639774d0e8ced048313b3c5ea8d66b7a035bcf35
KutsenkoAlexander <kucenkoaleksandr@gmail.com>
2021-02-08 23:29:27 +0200

    - adding a new page "Dialogs"; - improvement HTML and CSS;

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className="App">
             <Header/>
             <Sidebar/>
-            <Profile/>
+            <div className="wrapperContent">
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
         </div>
     );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,9 +4,9 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={styles.profile}>
+        <div>
             <div>
-                <img
+                <img className={styles.profileImg}
                     src="https://images.unsplash.com/photo-1496889748357-08060ad87d28?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2124&q=80"/>
             </div>
             <div>ava + description</div>
