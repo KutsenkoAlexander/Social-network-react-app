cb3ca7887fda6599f76b9cab18afc7ef52c4350c
KutsenkoAlexander <kucenkoaleksandr@gmail.com>
2021-02-09 23:19:19 +0200

    adding route for pages: profile, dialogs, news, music, settings

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Sidebar/>
-            <div className="wrapperContent">
-                <Dialogs/>
-                {/*<Profile/>*/}
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Sidebar/>
+                <div className="wrapperContent">
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
