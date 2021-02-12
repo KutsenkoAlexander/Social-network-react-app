86b30b1ffd3ee627b073dbdbe055c71c1cf8dc10
KutsenkoAlexander <kucenkoaleksandr@gmail.com>
2021-02-12 23:20:20 +0200

    adding Dialogs markup and some CSS classes

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,19 @@ import styles from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return(
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogItems}>
+                <div className={styles.dialog + ' ' + styles.active}>Bob</div>
+                <div className={styles.dialog}>Marty</div>
+                <div className={styles.dialog}>Tom</div>
+                <div className={styles.dialog}>Ben</div>
+                <div className={styles.dialog}>Barbara</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi!</div>
+                <div className={styles.message}>Morning!</div>
+                <div className={styles.message}>Yo!</div>
+            </div>
         </div>
     );
 }
