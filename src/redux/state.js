b7335d53529b1aeb9bf2ed7689286b201482c37b
KutsenkoAlexander <kucenkoaleksandr@gmail.com>
2021-02-14 18:49:40 +0200

    Improving state for whole application. Adding update state for input into text area for new post and new message.

--- src/redux/state.js
+++ src/redux/state.js
@@ -5,21 +5,43 @@ let state = {
         posts: [
             {id: 1, text: 'Hello World!', likeCount: 10},
             {id: 2, text: 'It\'s second post.', likeCount: 3}
-        ]
+        ],
+        newPostText: ''
     },
     dialogPage: {
         dialogs: [
-            {id: 1, name: 'Bob', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'},
-            {id: 2, name: 'Marty', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'},
-            {id: 3, name: 'Tom', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'},
-            {id: 4, name: 'Jerry', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'},
-            {id: 5, name: 'Barbara', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'}
+            {
+                id: 1,
+                name: 'Bob',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            },
+            {
+                id: 2,
+                name: 'Marty',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            },
+            {
+                id: 3,
+                name: 'Tom',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            },
+            {
+                id: 4,
+                name: 'Jerry',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            },
+            {
+                id: 5,
+                name: 'Barbara',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            }
         ],
         messages: [
             {id: 1, text: 'Hello'},
             {id: 2, text: 'Morning!'},
             {id: 3, text: 'Yo'}
-        ]
+        ],
+        newMessageText: ''
     },
     sidebar: {
         navigation: [
@@ -30,32 +52,64 @@ let state = {
             {path: '/settings', name: 'Settings'}
         ],
         friends: [
-            {id: 1, name: 'Kim', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'},
-            {id: 2, name: 'Tom', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'},
-            {id: 3, name: 'Rich', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'},
-            {id: 4, name: 'Mike', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'}
+            {
+                id: 1,
+                name: 'Kim',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            },
+            {
+                id: 2,
+                name: 'Tom',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            },
+            {
+                id: 3,
+                name: 'Rich',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            },
+            {
+                id: 4,
+                name: 'Mike',
+                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7IdJ1FLinVuz9qnO_OkkJArH8xsRkXIOv8w&usqp=CAU'
+            }
         ]
     }
 }
 
-export let addPost = (text) => {
+//--------------------------
+//---------- Post ----------
+//--------------------------
+export let addPost = () => {
     let posts = state.profilePage.posts;
     let newPost = {
         id: posts.length + 1,
-        text: text,
+        text: state.profilePage.newPostText,
         likeCount: 0
     };
     posts.push(newPost);
-    rerenderEntireTree(state);
+    updateNewPostText('');
 };
 
-export let sendMessage = (text) => {
+export let updateNewPostText = (text) => {
+    state.profilePage.newPostText = text;
+    rerenderEntireTree(state);
+}
+
+//--------------------------
+//-------- Message ---------
+//--------------------------
+export let sendMessage = () => {
     let messages = state.dialogPage.messages;
     let newMessage = {
         id: messages.length + 1,
-        text: text
+        text: state.dialogPage.newMessageText
     };
     messages.push(newMessage);
+    updateNewMessageText('');
+}
+
+export let updateNewMessageText = (text) => {
+    state.dialogPage.newMessageText = text;
     rerenderEntireTree(state);
 }
 
