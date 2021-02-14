80b29a40e232e731fadff33d3e3912b046058eaf
KutsenkoAlexander <kucenkoaleksandr@gmail.com>
2021-02-14 14:06:48 +0200

    improving code, adding onClick function for creating new post and message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,10 +4,20 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-    let dialogsElements = props.state.dialogs.map(dialog => <DialogItem id={dialog.id}
-                                                                        name={dialog.name}
-                                                                        avatar={dialog.avatar}/>);
-    let messagesElement = props.state.messages.map(message => <Message id={message.id} text={message.text}/>)
+    let dialogsElements =
+        props.state.dialogs.map(dialog => <DialogItem id={dialog.id}
+                                                      name={dialog.name}
+                                                      avatar={dialog.avatar}/>);
+    let messagesElement =
+        props.state.messages.map(message => <Message id={message.id}
+                                                     text={message.text}/>)
+
+    let newMessageElement = React.createRef();
+
+    let sendMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
 
     return (
         <div className={styles.dialogs}>
@@ -15,7 +25,11 @@ const Dialogs = (props) => {
                 {dialogsElements}
             </div>
             <div className={styles.messages}>
-                {messagesElement}
+                <div>{messagesElement}</div>
+                <div className={styles.newMessage}>
+                    <textarea ref={newMessageElement}/>
+                    <button onClick={sendMessage}>Send</button>
+                </div>
             </div>
         </div>
     );
