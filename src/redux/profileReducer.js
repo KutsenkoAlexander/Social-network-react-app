29176be09a99d1946cee4f0463db48a6809df18e
KutsenkoAlexander <kucenkoaleksandr@gmail.com>
2021-02-28 17:39:04 +0200

    providing copy of state for rendering UI after changed state

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -11,19 +11,24 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let posts = state.posts;
             let newPost = {
                 id: posts.length + 1,
                 text: state.newPostText,
                 likeCount: 0
             };
-            posts.push(newPost);
-            state.newPostText = '';
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.text;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = '';
+            return stateCopy;
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy = {...state};
+            stateCopy.newPostText = action.text;
+            return stateCopy;
+        }
         default:
             return state;
     }
