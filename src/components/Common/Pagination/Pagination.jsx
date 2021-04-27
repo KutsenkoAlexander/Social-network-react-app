567461d20f5b40252e5105aef4eadcd0814d1336
KutsenkoAlexander <kucenkoaleksandr@gmail.com>
2021-04-28 00:01:27 +0300

    providing advanced paginator

--- src/components/Common/Pagination/Pagination.jsx
+++ src/components/Common/Pagination/Pagination.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Pagination.module.css'
 
-const Pagination = React.memo(({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
-    // let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    let pagesCount = Math.ceil(100 / pageSize);
+const Pagination = React.memo(({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize=20}) => {
+    let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
     let pages = [];
     for (let i=1; i<=pagesCount; i++) {
         pages.push(i);
     }
 
+    let[portionNumber, setPortionNumber] = useState(1);
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+
     return <div className={styles.pagination}>
-            {
-                pages.map(pageNumber => {
-                    return <span
-                        key={pageNumber}
-                        className={currentPage === pageNumber ? styles.selectedPage : ''}
-                        onClick={() => { onPageChanged(pageNumber) }}>
-                            {pageNumber}
-                        </span>;
-                })
-            }
+        { portionNumber > 1 && <button onClick={() => setPortionNumber(portionNumber - 1)}>PREV</button> }
+        { pages
+            .filter(page => page >= leftPortionPageNumber && page <= rightPortionPageNumber)
+            .map(pageNumber => {
+                return <span
+                    key={pageNumber}
+                    className={currentPage === pageNumber ? styles.selectedPage : ''}
+                    onClick={() => { onPageChanged(pageNumber) }}>
+                    {pageNumber}
+                </span>;
+            })
+        }
+        { portionCount > portionNumber && <button onClick={() => setPortionNumber(portionNumber + 1)}>NEXT</button> }
         </div>
 })
 
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -11,7 +11,7 @@ const TOGGLE_FOLLOWING_PROGRESS = 'TOGGLE_FOLLOWING_PROGRESS';
 
 let initialState = {
     users: [],
-    pageSize: 3,
+    pageSize: 10,
     totalUsersCount: 0,
     currentPage: 1,
     isFetching: true,
